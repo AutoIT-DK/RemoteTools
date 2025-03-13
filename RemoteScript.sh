761490779e51498b437f4cbe0350550121842996
Morten Gryning <69143423+mgryning@users.noreply.github.com>
2025-03-13 15:52:58 +0100

    Update RemoteScript.sh

--- RemoteScript.sh
+++ RemoteScript.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 
-# This is the remote script downloaded and run by your startup script.
-# The key is the VERSION line below—your startup script looks for it
-# to decide whether it’s a "new" version.
-
 VERSION="1.2.5"
 
-# Example commands you want to run just once per version:
 echo "Remote scripts initialized. Version $VERSION!"
 
 # Maybe you want to install something:
