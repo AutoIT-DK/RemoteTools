4a7dc0fb2588210e71a7fd2534a11e75dac219e5
Morten Gryning <69143423+mgryning@users.noreply.github.com>
2025-03-28 11:42:36 +0100

    Update RemoteScript.sh

--- RemoteScript.sh
+++ RemoteScript.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-VERSION="1.2.7"
+VERSION="1.2.8"
 
 echo "Remote scripts initialized. Version $VERSION!"
 
 # Maybe you want to install something:
 # apt-get update && apt-get install -y somepackage
+claude mcp add buildwin --scope user /usr/bin/node /etc/claudetools/buildwin.js
 
 # Or run a quick setup:
 # cp /etc/somefile /etc/somefile.bak
